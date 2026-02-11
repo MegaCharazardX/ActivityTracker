2b69a833f59bba3f8c60863863a5e7d38aa94707
MegaCharazardX <haridhejus@gmail.com>
2026-02-11 17:33:36 +0530

    INIT COMMIT

---
+++ index.py
@@ -0,0 +1,24 @@
+import flask
+import requests
+
+base_url = "https://pokeapi.co/api/v2/"
+
+def get_pokemon_info(name) :
+    url= f"{base_url}/pokemon/{name}"
+    response = requests.get(url)
+    #print(response)
+    
+    if response.status_code == 200:
+        print("Data Retrieved")
+        pokemon_data = response.json()
+        return pokemon_data
+    else:
+        print(f"Request Failed")
+    
+pokemon_name = "charizard"
+pokemon_info = get_pokemon_info(pokemon_name)
+
+if pokemon_info :
+    print(f"{pokemon_info["name"]}")
+    print(f"{pokemon_info["abilities"]}")
+    print(f"{pokemon_info["types"]}")
